2eeae2f74d176edd7cddc1ef60244aebcef87994
k1GG <112746690+k1GG@users.noreply.github.com>
2026-02-14 12:16:27 +0530

    Create Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <h1>Sentinel Sync CRM</h1>
+      <nav>
+        {/* Add your navigation links here */}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
